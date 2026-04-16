d3e0aa6645ddf91fd5089302b8fc49a727ce5a12
am2525 <tear163406290741@gmail.com>
2026-04-16 11:11:39 +0900

    update: 内容更新

---
+++ js_learning/js/if.js
@@ -0,0 +1,6 @@
+// TODO: ユーザー配列(条件)の設定
+// userID: kababwrap
+// PassWord: kababkabab
+// TODO: 配列と一致していないときエラーメッセージを表示
+// TODO: IDが8文字以下の時、「8文字以上で記入してください。」と表示
+// TODO: PassWordが8文字以下と英数字以外の時、「8文字以上で記入してください。」と表示
