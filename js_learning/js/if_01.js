bc56763d91864c66618072dcdf1cbab6237b0665
am241012 <tear163406290741@gmail.com>
2026-04-19 09:48:26 +0900

    TODO: 課題文

---
+++ js_learning/js/if_01.js
@@ -0,0 +1,11 @@
+// emailとpasswordの入力バリデーションチェックのフォーム作成課題
+// branch名: feature/form
+
+// email: kababrawp@kabab.com
+// password: kabab0000
+
+// 共通 未入力になったら「必須項目です」と表示する
+// emailの入力形式以外の場合「emailの入力形式で記載してください」と表示する
+// 上記のemailではない場合「emailが違います」と表示する
+// passwordの条件「英数字8文字以上」を満たしていない場合「英数字8文字以上で記載してください」と表示する
+// 上記のpasswordではない場合「passwordが違います」と表示する
